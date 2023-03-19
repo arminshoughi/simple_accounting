dc49c90aa058520d32371eef76a40c3fd700c69b
armin shoghi <a.shoghi@cpol.co>
2023-03-20 00:10:43 +0330

    refactored UserRelatedDataRestricted

--- backend/accounting/utils/viewsets.py
+++ backend/accounting/utils/viewsets.py
@@ -1,11 +1,3 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-
-
-class UserRelatedDataModelViewSet(ModelViewSet):
-    queryset = None
-    serializer_class = None
-    permission_classes = [IsAuthenticated]
-
+class UserRelatedDataRestricted(object):
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
