518c82db0848339a69849201d54eeff67838acf5
armin shoghi <a.shoghi@cpol.co>
2023-03-20 13:12:41 +0330

    update requirements.txt and bug-fix

--- backend/accounting/accounting/urls.py
+++ backend/accounting/accounting/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
