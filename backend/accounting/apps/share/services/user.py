e5f6986579fefba3542131414bc19502cd0103a8
armin shoghi <a.shoghi@cpol.co>
2023-03-20 12:29:02 +0330

    added create_user method in UserService

--- backend/accounting/apps/share/services/user.py
+++ backend/accounting/apps/share/services/user.py
@@ -6,3 +6,10 @@ UserModel = get_user_model()
 
 class UserService(BaseService):
     model = UserModel
+
+    @classmethod
+    def create_user(cls, username, password, **kwargs):
+        user = cls.model(username=username, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
