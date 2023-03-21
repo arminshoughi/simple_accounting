b025371f1317780e14829d212b1196c41ff02508
armin shoghi <a.shoghi@cpol.co>
2023-03-21 15:09:19 +0330

    change database engine to postgresql

--- backend/accounting/accounting/settings/production.py
+++ backend/accounting/accounting/settings/production.py
@@ -1,15 +1,5 @@
 from .base import *
 
 ALLOWED_HOSTS = ['accounting.ir']
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'accounting_db',
-        "USER": 'accounting_user',
-        "PASSWORD": 'accounting_password@',
-        "HOST": 'localhost',
-        "PORT": "5432",
-    }
-}
 
 DEBUG = False
